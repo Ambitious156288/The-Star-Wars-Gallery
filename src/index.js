e27bcbb414c86dc41375218b43054c5905ceb99f
Ambitious156288 <ambitious156288@gmail.com>
2021-06-01 01:03:28 +0100

    add API endpoints

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles/style.scss";
+
+class StarWarsCatalog {
+  constructor() {
+    this.cards = [];
+    this.newCars = [];
+
+    this.API = "https://swapi.dev/api";
+    this.API_RESOURCE = "people";
+
+    this.API_ENDPOINT = `${this.API}/${this.API_RESOURCE}`;
+  }
+}
