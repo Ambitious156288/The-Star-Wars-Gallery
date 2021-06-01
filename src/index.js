55927a317882e4451107554e6da59de0d9e3f9e6
Ambitious156288 <ambitious156288@gmail.com>
2021-06-01 03:55:00 +0100

    display another data in Card component

--- src/index.js
+++ src/index.js
@@ -15,13 +15,13 @@ class StarWarsCatalog {
     };
   }
 
-  initializeCatalog() {
+  initializePeople() {
     this.catalog = document.querySelector(this.UiSelectors.content);
 
-    this.pullCards();
+    this.pullPeople();
   }
 
-  async pullCards() {
+  async pullPeople() {
     const { results } = await this.fetchData(this.API_ENDPOINT);
 
     this.people = [...results];
@@ -39,9 +39,44 @@ class StarWarsCatalog {
   }
 
   createCatalog(people) {
-    this.catalog.innerHTML += [people.map((person) => person.name)];
+    this.catalog.innerHTML += [
+      people.map((person) => this.createPerson(person)),
+    ];
+  }
+
+  createPerson({
+    name,
+    height,
+    mass,
+    hair_color,
+    skin_color,
+    eye_color,
+    birth_year,
+    gender,
+  }) {
+    return `
+        <div class="row">
+        <div class="col s12 m4">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">${name}</span>
+            </div>
+            <div class="card-action">
+            <p>height: ${height}</p>
+            <p>mass: ${mass}</p>
+            <p>height: ${hair_color}</p>
+            <p>mass: ${skin_color}</p>
+            <p>height: ${height}</p>
+            <p>mass: ${eye_color}</p>
+            <p>height: ${birth_year}</p>
+            <p>mass: ${gender}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
   }
 }
 
 const catalog = new StarWarsCatalog();
-catalog.initializeCatalog();
+catalog.initializePeople();
